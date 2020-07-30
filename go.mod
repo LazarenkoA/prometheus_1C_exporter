9875e7d07b143b1e4352656e7c3189fd6e78ae7e
Daniel Podolsky <1049874+onokonem@users.noreply.github.com>
2020-07-30 11:36:15 +0300

    fix: go mod compatibility

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/LazarenkoA/prometheus_1C_exporter
+
+go 1.14
+
+require (
+	github.com/LazarenkoA/LogrusRotate v1.0.1-0.20200420172000-d4b9e48cb178
+	github.com/matishsiao/goInfo v0.0.0-20200404012835-b5f882ee2288
+	github.com/prometheus/client_golang v1.7.1
+	github.com/prometheus/procfs v0.1.3
+	github.com/sirupsen/logrus v1.6.0
+	golang.org/x/sys v0.0.0-20200728102440-3e129f6d46b1 // indirect
+	gopkg.in/yaml.v2 v2.3.0
+)
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/sirupsen/logrus"
 	"math/rand"
 	"net/http"
 	"os"
@@ -12,8 +11,7 @@ import (
 	"strings"
 	"syscall"
 
-	. "prometheus_1C_exporter/explorers"
-	"time"
+	"github.com/sirupsen/logrus"
 
 	logrusRotate "github.com/LazarenkoA/LogrusRotate"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -37,7 +35,7 @@ func main() {
 		return
 	}
 
-	//settingsPath = "D:\\GoMy\\src\\prometheus_1C_exporter\\settings.yaml" // debug
+	// settingsPath = "D:\\GoMy\\src\\prometheus_1C_exporter\\settings.yaml" // debug
 	s := loadSettings(settingsPath)
 
 	lw := new(logrusRotate.Rotate).Construct()
@@ -102,13 +100,13 @@ func main() {
 	for err := range cerror {
 		logrusRotate.StandardLogger().WithError(err).Error()
 		fmt.Printf("Произошла ошибка:\n\t%v", err)
-		//break
+		// break
 	}
 
 }
 
-///////////////// RotateConf ////////////////////////////////////////////////////
-func (w *RotateConf) Construct(s *settings ) *RotateConf {
+// /////////////// RotateConf ////////////////////////////////////////////////////
+func (w *RotateConf) Construct(s *settings) *RotateConf {
 	w.settings = s
 	return w
 }
@@ -133,6 +131,5 @@ func (w *RotateConf) TimeRotate() int {
 	return w.settings.TimeRotate
 }
 
-
 // go build -o "Explorer_1C" -ldflags "-s -w" - билд чутка меньше размером
-//ansible app_servers -m shell -a  "systemctl stop 1c_exporter.service && yes | cp /mnt/share/GO/prometheus_1C_exporter/1c_exporter /usr/local/bin/1c_exporter &&  systemctl start 1c_exporter.service"
+// ansible app_servers -m shell -a  "systemctl stop 1c_exporter.service && yes | cp /mnt/share/GO/prometheus_1C_exporter/1c_exporter /usr/local/bin/1c_exporter &&  systemctl start 1c_exporter.service"
