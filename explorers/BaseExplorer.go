cf6b257e80c351989c54721fb8705acf206d78f4
Lazarenko.AN <Lazarenko.AN@parma.ru>
2020-08-20 16:29:36 +0300

    багфикс + больше логирования

--- explorers/BaseExplorer.go
+++ explorers/BaseExplorer.go
@@ -151,8 +151,8 @@ func (this *BaseExplorer) Stop() {
 }
 
 func (this *BaseExplorer) Pause() {
-	logrusRotate.StandardLogger().Trace("Pause begin")
-	defer logrusRotate.StandardLogger().Trace("Pause end")
+	logrusRotate.StandardLogger().Trace("Pause. begin")
+	defer logrusRotate.StandardLogger().Trace("Pause. end")
 
 	if this.summary != nil {
 		this.summary.Reset()
@@ -163,12 +163,21 @@ func (this *BaseExplorer) Pause() {
 
 	if atomic.CompareAndSwapInt32(&this.isLocked, 0, 1) { // нужно что бы 2 раза не наложить lock
 		this.Lock()
+		logrusRotate.StandardLogger().Trace("Pause. Блокировка установлена")
+	} else {
+		logrusRotate.StandardLogger().WithField("isLocked", this.isLocked).Trace("Pause. Уже заблокировано")
 	}
 }
 
 func (this *BaseExplorer) Continue() {
-	if atomic.CompareAndSwapInt32(&this.isLocked, 1, -1)  {
+	logrusRotate.StandardLogger().Trace("Continue. begin")
+	defer logrusRotate.StandardLogger().Trace("Continue. end")
+
+	if atomic.CompareAndSwapInt32(&this.isLocked, 1, 0)  {
 		this.Unlock()
+		logrusRotate.StandardLogger().Trace("Continue. Блокировка снята")
+	} else {
+		logrusRotate.StandardLogger().WithField("isLocked", this.isLocked).Trace("Continue. Блокировка не была установлена")
 	}
 }
 
@@ -283,14 +292,22 @@ func Pause(metrics *Metrics) http.Handler {
 			http.Error(w,  fmt.Sprintf("Метод %q не поддерживается", r.Method), http.StatusInternalServerError)
 			return
 		}
+		logrusRotate.StandardLogger().WithField("URL", r.URL.RequestURI()).Trace("Пауза")
+
 		metricNames := r.URL.Query().Get("metricNames")
+		offsetMinStr := r.URL.Query().Get("offsetMin")
+
 		var offsetMin int
-		if v, err := strconv.ParseInt(r.URL.Query().Get("offsetMin"), 0, 0); err == nil {
-			offsetMin = int(v)
-			logrusRotate.StandardLogger().Info("Сбор метрик включится автоматически через ", offsetMin, "минут")
+		if offsetMinStr != "" {
+			if v, err := strconv.ParseInt(offsetMinStr, 0, 0); err == nil {
+				offsetMin = int(v)
+				logrusRotate.StandardLogger().Infof("Сбор метрик включится автоматически через %d минут", offsetMin)
+			} else {
+				logrusRotate.StandardLogger().WithError(err).WithField("offsetMin", offsetMinStr).Error("Ошибка конвертации offsetMin")
+			}
 		}
 
-		logrusRotate.StandardLogger().Info("Приостановить сбор метрик ", metricNames)
+		logrusRotate.StandardLogger().Infof("Приостановить сбор метрик %q", metricNames)
 		for _, metricName := range strings.Split(metricNames, ",") {
 			if exp := metrics.findExplorer(metricName); exp != nil {
 				exp.Pause()
@@ -316,6 +333,8 @@ func Continue(metrics *Metrics) http.Handler {
 			http.Error(w,  fmt.Sprintf("Метод %q не поддерживается", r.Method), http.StatusInternalServerError)
 			return
 		}
+		logrusRotate.StandardLogger().WithField("URL", r.URL.RequestURI()).Trace("Продолжить")
+
 		metricNames := r.URL.Query().Get("metricNames")
 		logrusRotate.StandardLogger().Info("Продолжить сбор метрик", metricNames)
 		for _, metricName := range strings.Split(metricNames, ",") {
