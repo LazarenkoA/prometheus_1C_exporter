ec48db70aef1c4f3c28a6b78afce5916b328264d
Сергей Долинин <imhunter74@gmail.com>
2025-10-28 00:23:31 +0500

    Поправил вывод экземпляра

--- explorers/exporterSessionsData.go
+++ explorers/exporterSessionsData.go
@@ -155,20 +155,20 @@ func (exp *ExporterSessionsMemory) getValue() {
 			withExemplar := v.GetWith("id", "user")
 			for n, m := range v.metersData {
 				hist := exp.histograms[n]
-				if exp.usedExemplars() && cntExml == 0 {
+				if exp.usedExemplars() && cntExml == 0 && m != 0 {
 					hist.With(withLabel).(prometheus.ExemplarObserver).ObserveWithExemplar(float64(m), withExemplar)
 				} else {
 					hist.With(withLabel).Observe(float64(m))
+					if exp.countsExemplars[v.labelsData["base"]] == nil {
+						exp.countsExemplars[v.labelsData["base"]] = map[string]int{}
+					}
+					cntExml++
+					if cntExml >= 10 {
+						cntExml = 0
+					}
+					exp.countsExemplars[v.labelsData["base"]][v.labelsData["appid"]] = cntExml
 				}
 			}
-			if exp.countsExemplars[v.labelsData["base"]] == nil {
-				exp.countsExemplars[v.labelsData["base"]] = map[string]int{}
-			}
-			cntExml++
-			if cntExml >= 10 {
-				cntExml = 0
-			}
-			exp.countsExemplars[v.labelsData["base"]][v.labelsData["appid"]] = cntExml
 		}
 		delete(exp.buff, k)
 	}
